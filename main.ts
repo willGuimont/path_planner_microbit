a59014530dc31d7e3cc814b4ee1df8f68e02245b
William Guimont-Martin <willGuimont@users.noreply.github.com>
2021-01-14 19:31:13 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+let world = [
+    [0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0]]
+
+let position = (0, 0)
+const WORLD_WIDTH = 5
+const WORLD_HEIGHT = 5
+
+for (let i = 0; i < WORLD_WIDTH; ++i) {
+    for (let j = 0; j < WORLD_HEIGHT; ++j) {
+        
+    }
+}
+
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    . . # . .
+    . . . # .
+    . . . . #
+    `)
